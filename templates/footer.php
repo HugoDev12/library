864eb629beb9ba27f1183a78867ee13d34af7707
AxelDuval <axel.duval@gmail.com>
2022-02-01 09:19:32 +0100

    Add Header and Footer

---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<footer class="p-3 footer-light bg-light">
+      <div class="container text-center">
+        <span class="text-muted ">Bibliothèque - Copyright 2021</span>
+      </div>
+    </footer>
