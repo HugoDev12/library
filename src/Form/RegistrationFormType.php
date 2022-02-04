2fe5afd73b3f7a52d044caaae7f4bed8a9e4ee00
Occelli <occelli.hugo@hotmail.fr>
2022-02-04 11:02:06 +0100

    login added and conflicts resolved

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -12,13 +12,15 @@ use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, ['attr' => ["class" => "form-control email"]])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
