7f9fa228a493113beedcbf84c252cbfdc31a068b
Laurina Toussaint <laurinatoussaint@gmail.com>
2022-02-05 17:06:54 +0100

    switch off nav from login

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
-<footer class="mt-2 p-4 footer-light bg-light">
+<footer class="mt-5 p-4 footer-light bg-light">
       <div class="container text-center">
         <span class="text-muted ">Bibliothèque - Copyright 2021</span>
       </div>
-    </footer>
+</footer>
